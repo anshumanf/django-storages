0948ca44517819b176721f8cdbc6ccf0e248700b
Anshuman Fotedar <Anshuman.Fotedar@worldquant.com>
2018-10-22 15:15:24 -0400

    Try to avoid a None value for content_type

--- storages/backends/s3boto3.py
+++ storages/backends/s3boto3.py
@@ -422,6 +422,8 @@ class S3Boto3Storage(Storage):
         content_type = getattr(content, 'content_type',
                                _type or self.default_content_type)
 
+        content_type = content_type or _type or self.default_content_type
+
         # setting the content_type in the key object is not enough.
         parameters.update({'ContentType': content_type})
 
